3165b067931bc5329331086ec76f2c40653d00d0
David Precious <davidp@preshweb.co.uk>
2012-09-04 22:18:58 +0100

    Look at attributes to see if login is required.
    Moving towards this part working.  Still a long, long way to Tipperary^Wgo.

--- lib/Dancer/Plugin/Auth/Extensible.pm
+++ lib/Dancer/Plugin/Auth/Extensible.pm
@@ -2,8 +2,16 @@ package Dancer::Plugin::Auth::Extensible;
 
 use warnings;
 use strict;
+use attributes;
+use Dancer::Plugin;
+use Dancer qw(:syntax);
+use Scalar::Util qw(refaddr);
 
 our $VERSION = '0.01';
+
+# We must export these to the caller to allow them to use any attributes:
+use Exporter 'import';
+our @EXPORT=qw(MODIFY_CODE_ATTRIBUTES FETCH_CODE_ATTRIBUTES);
 
 =head1 NAME
 
@@ -19,8 +27,7 @@ See for example L<Dancer::Plugin::Auth::Extensible::Provider::Database>.
     plugins:
         auth:
             extensible:
-                provider: Database
-                table: users
+                provider: Eample
 
 
 Define that a user must be logged in to access a route:
@@ -65,10 +72,93 @@ C<permission_denied> hook will fire; code using that hook can return a redirect
 or a response.  If no code does so, a default "permission denied" response will
 be issued.
 
+=cut 
+
+
+
+sub logged_in_user {
+    # TODO: write this
+}
+register logged_in_user => \&logged_in_user;
+
+register_plugin versions => qw(1 2);
+
+# Hook to catch routes about to be executed, and check for attributes telling us
+# we need to make sure the user is auth'd
+
+hook before => sub {
+    my $route_handler = shift || return;
+
+    my $requires_login = get_attribs_by_type(
+        'RequireLogin', $route_handler->code
+    );
+    if (defined $requires_login && !logged_in_user) {
+        # TODO: fire hooks, print default message
+        die "USER NOT LOGGED IN";
+    }
+
+    my $roles_required = get_attribs_by_type(
+        'RequireRole', $route_handler->code
+    );
+    return unless defined $roles_required;
+
+    # TODO: ensure the user has a suitable role
+    die "USER NEEDS ONE OF: " . join ',', @$roles_required;
+};
+
+
+# Boilerplate to support attribute setting & fetching
+my %attrs;
+sub MODIFY_CODE_ATTRIBUTES {
+    my ($package, $subref, @attrs) = @_;
+    $attrs{ refaddr $subref } = \@attrs;
+    return;
+} 
+sub FETCH_CODE_ATTRIBUTES {
+    my ($package, $subref) = @_;
+    my $attrs = $attrs{ refaddr $subref };
+    return $attrs ? @$attrs : ();
+}
+
+sub get_attribs_by_type {
+    my ($type, $coderef) = @_;
+    return unless $coderef;
+
+    warn "Want attributes for $coderef  - " . ref $coderef;
+
+    # This voodoo was originally written by an evil bad man with a big beard;
+    # I simply embraced and extended it, whilst midly fearing for my life.
+    # Thus, blame him, not me.
+
+    my @desired_attribs = grep { 
+        /^$type(?:\([^)]*\))?$/ 
+    } attributes::get($coderef);
+    
+    return if !@desired_attribs;
+
+    # OK, we matched an attribute above; it might have been on its own (e.g. 
+    # "LoginNeeded") or it might contain a list of values we need to return
+    # (e.g. RequireRole(Foo Bar Baz)).
+    # So, an empty arrayref is fine to return; it indicates we found the
+    # desired attribute, but it had no values within parens.
+    return [
+        map {
+            my $f = $_;
+            # extract and split a white-space-delimited list wrapped in
+            # parens with optional leading/trailing shitespace
+            $f =~ s/^$type\(\s*([^)]*)\s*\)$/$1/;
+            split(/\s+/, $f);
+        } @desired_attribs
+    ];
+}
+ 
+
+
 
 =head1 AUTHOR
 
 David Precious, C<< <davidp at preshweb.co.uk> >>
+
 
 =head1 BUGS / FEATURE REQUESTS
 
