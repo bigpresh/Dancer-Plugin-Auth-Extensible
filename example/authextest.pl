ef26e8ec6b6601700beb409601d40a54d80dea27
David Precious <davidp@preshweb.co.uk>
2012-09-05 00:06:49 +0100

    Example app to demonstrate the plugin in use.

---
+++ example/authextest.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+use Dancer;
+use Dancer::Plugin::Auth::Extensible;
+
+get '/' => sub {
+    my $content = "<h1>Non-secret home page!</h1>";
+    if (my $user = logged_in_user()) {
+        $content .= "<p>Hi there, $user->{name}!</p>";
+    } else {
+        $content .= "<p>Why not log in?</p>";
+    }
+
+    $content .= <<LINKS;
+<p><a href="/secret">Psst, wanna know a secret?</a></p>
+<p><a href="/beer">Or maybe you want a beer</a></p>
+<p><a href="/vodka">Or, a vodka?</a></p>
+LINKS
+    return $content;
+};
+
+get '/secret' => sub :RequireLogin() { "Need to be logged in" };
+
+get '/beer' => sub :RequireRole(BeerDrinker HardDrinker) {
+    "Any drinker can get beer.";
+};
+
+get '/vodka' => sub :RequireRole(HardDrinker) {
+    "Only hard drinkers get vodka";
+};
+
+dance();
