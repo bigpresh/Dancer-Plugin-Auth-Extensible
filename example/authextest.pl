32f231eff1f4db3df686f0ee24967259de9fb723
David Precious <davidp@preshweb.co.uk>
2012-09-05 16:24:14 +0100

    Demonstrate user_has_role in example app.

--- example/authextest.pl
+++ example/authextest.pl
@@ -16,6 +16,14 @@ get '/' => sub {
 <p><a href="/beer">Or maybe you want a beer</a></p>
 <p><a href="/vodka">Or, a vodka?</a></p>
 LINKS
+
+    if (user_has_role('BeerDrinker')) {
+        $content .= "<p>You can drink beer</p>";
+    }
+    if (user_has_role('WineDrinker')) {
+        $content .= "<p>You can drink wine</p>";
+    }
+
     return $content;
 };
 
