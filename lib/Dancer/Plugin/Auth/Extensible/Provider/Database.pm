1fbeb90975a572215e0afb6756fd4e60fd7b9a3d
David Precious <davidp@preshweb.co.uk>
2012-09-07 00:04:03 +0100

    Start of Database authentication provider.
    Documented the design mostly, and implemented (but not tested) basic user
    authentication.

---
+++ lib/Dancer/Plugin/Auth/Extensible/Provider/Database.pm
@@ -0,0 +1,245 @@
+package Dancer::Plugin::Auth::Extensible::Provider::Database;
+
+use strict;
+use Dancer::Plugin;
+use Dancer::Plugin::Database;
+use Dancer qw(:syntax);
+
+my $settings = plugin_setting();
+
+=head1 NAME 
+
+Dancer::Plugin::Auth::Extensible::Database - authenticate via a database
+
+
+=head1 DESCRIPTION
+
+This class is an authentication provider designed to authenticate users against
+a database, using L<Dancer::Plugin::Database> to access a database.
+
+L<Crypt::SaltedHash> is used to handle hashed passwords securely; you wouldn't
+want to store plain text passwords now, would you?  (If your answer to that is
+yes, please reconsider; you really don't want to do that, when it's so easy to
+do things right!)
+
+See L<Dancer::Plugin::Database> for how to configure a database connection
+appropriately; see the L</CONFIGURATION> section below for how to configure this
+authentication provider with database details.
+
+See L<Dancer::Plugin::Auth::Extensible> for details on how to use the
+authentication framework, including how to pick a more useful authentication
+provider.
+
+
+=head1 CONFIGURATION
+
+This provider tries to use sensible defaults, so you may not need to provide
+much configuration if your database tables look similar to those in the
+L</SUGGESTED SCHEMA> section below.
+
+The most basic configuration, assuming defaults for all options:
+
+    plugins:
+        Auth::Extensible:
+            provider: 'Database'
+
+A full example showing all options:
+
+    plugins:
+        Auth::Extensible:
+            provider: 'Database'
+            # optionally set DB connection name to use (see named connections in
+            # Dancer::Plugin::Database docs)
+            db_connection_name: 'foo'
+
+            # Optionally disable roles support, if you only want to check for
+            # successful logins but don't need to use role-based access:
+            disable_roles: 1
+
+            # optionally specify names of tables if they're not the defaults
+            # (defaults are 'users', 'roles' and 'user_roles')
+            users_table: 'users'
+            roles_table: 'roles'
+            user_roles_table: 'user_roles'
+
+            # optionally set the column names (see the SUGGESTED SCHEMA section
+            # below for the default names; if you use them, they'll Just Work)
+            users_id_column: 'id'
+            users_username_column: 'username'
+            users_password_column: 'password'
+            roles_id_column: 'id'
+            roles_role_column: 'role'
+            user_roles_user_id_column: 'user_id'
+            user_roles_role_id_column: 'roles_id'
+
+
+
+=head1 SUGGESTED SCHEMA
+
+If you use a schema similar to the examples provided here, you should need
+minimal configuration to get this authentication provider to work for you.
+
+The examples given here should be MySQL-compatible; minimal changes should be
+required to use them with other database engines.
+
+=head2 users table
+
+You'll need a table to store user accounts in, of course.  A suggestion is
+something like:
+
+    CREATE TABLE users (
+        id       INTEGER     AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(32) NOT NULL       UNIQUE KEY,
+        password VARCHAR(32) NOT NULL
+    );
+
+You will quite likely want other fields to store e.g. the user's name, email
+address, etc; all columns from the users table will be returned by the
+C<logged_in_user> keyword for your convenience.
+
+=head2 roles table
+
+You'll need a table to store a list of available roles in (unless you're not
+using roles - in which case, disable role support (see the L</CONFIGURATION>
+section).
+
+    CREATE TABLE roles (
+        id    INTEGER     AUTO_INCREMENT PRIMARY KEY,
+        role  VARCHAR(32) NOT NULL
+    );
+
+=head user_roles table
+
+Finally, (unless you've disabled role support)  you'll need a table to store
+user <-> role mappings (i.e. one row for every role a user has; so adding 
+extra roles to a user consists of adding a new role to this table).  It's 
+entirely up to you whether you use an "id" column in this table; you probably
+shouldn't need it.
+
+    CREATE TABLE user_roles (
+        user_id  INTEGER  NOT NULL,
+        role_id  INTEGER  NOT NULL
+    );
+    UNIQUE KEY user_role (user_id, role_id);
+
+If you're using InnoDB tables rather than the default MyISAM, you could add a
+foreign key constraint for better data integrity; see the MySQL documentation
+for details, but a table definition using foreign keys could look like:
+
+    CREATE TABLE user_roles (
+        user_id  INTEGER, FOREIGN KEY (user_id) REFERENCES users (id),
+        role_id  INTEGER, FOREIGN_KEY (role_id) REFERENCES roles (id)
+    ) ENGINE=InnoDB;
+    UNIQUE KEY user_role (user_id, role_id);
+
+
+=cut
+
+
+=head1 CLASS METHODS
+
+=item authenticate_user
+
+Given the username and password entered by the user, return true if they are
+authenticated, or false if not.
+
+=cut
+
+sub authenticate_user {
+    my ($class, $username, $password) = @_;
+
+    # Get our database handle and find out the table and column names:
+    my $database = database($settings->{db_connection_name});
+
+    my $users_table     = $settings->{users_table}     || 'users';
+    my $username_column = $settings->{username_column} || 'username';
+    my $password_column = $settings->{password_column} || 'password';
+
+    # Look up the user, 
+    my $user = $database->quick_select(
+        $users_table, { $username_col => $username }
+    );
+    if (!$user) {
+        debug("No such user $username");
+        return;
+    }
+
+    # Now, see if the password matches.  Try a direct comparison first, then try
+    # comparing using Crypt::SaltedHash.  TODO: perhaps check if it looks
+    # anything like a hashed password first?
+    my $password_match;
+    my $check_case = $settings->{case_sensitive_password} || 0;
+    $password_match = $check_case
+        ?    $password eq    $user->{$password_col}
+        : lc $password eq lc $user->{$password_col};
+
+    if (!$password_match) {
+        # OK, now try comparing via Crypt::SaltedHash
+        Crypt::SaltedHash->validate($user->{$password_column}, $password)
+            and $password_match++;
+    }
+
+    # Right, moment of truth: are they todays winner?
+    if ($password_match) {
+        debug("Successful login for $username");
+        return 1;
+    } else {
+        debug("Incorrect password for $username");
+        return;
+    }
+
+    # OK, the username and password are correct.  If we're not dealing with
+    # roles, then go no further:
+    return 1 if $settings->{disable_roles};
+
+
+    # Right, next, fetch the roles they have.  There's currently no support for
+    # JOINs in Dancer::Plugin::Database, so we'll need to do this query
+    # ourselves - so we'd better take care to quote the table & column names, as
+    # we're going to have to interpolate them.  (They're coming from our config,
+    # so should be pretty trustable, but they might conflict with reserved
+    # identifiers or have unacceptable characters to not be quoted.)
+    my $roles_table = $database->dbh->quote_identifier(
+        $settings->{roles_table} || 'roles'
+    );
+    my $user_roles_table = $database->dbh->quote_identifier(
+        $settings->{user_roles_table} || 'user_roles'
+    );
+    my $user_id = $database->dbh->quote_identifier($user->{
+
+
+}
+
+=item get_user_details
+
+Given a username, return details about the user.  The details returned will vary
+depending on the provider; some providers may be able to return various data
+about the user, some may not, depending on the authentication source.
+
+Details should be returned as a hashref.
+
+=cut
+
+sub get_user_details {
+    my ($class, $username) = @_;
+
+    return $users{lc $username};
+}
+
+=item get_user_roles
+
+Given a username, return a list of roles that user has.
+
+=cut
+
+sub get_user_roles {
+    my ($class, $username) = @_;
+
+    my $user_details = $class->get_user_details($username) or return;
+    return $user_details->{roles};
+}
+
+
+
+1;
+
