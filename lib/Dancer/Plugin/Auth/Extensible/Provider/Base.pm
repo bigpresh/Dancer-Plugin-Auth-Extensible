e21f71903f3ae6d819b4d9ba6b4aa21fcd0cbd97
David Precious <davidp@preshweb.co.uk>
2012-11-25 01:23:43 +0000

    Provider base class enhancements.
    Add password matching here, so various providers can use $self->match_password()
    and get crypted password handling for free.
    
    Add stub methods for the methods which providers must implement, which will die
    if they get called - so if a provider does not override a required method, it
    will be clear what the issue is, rather than just exploding.
    
    Handle the realm settings being passed to the constructor, stored in the object,
    and add a `realm_settings` accessor.

--- lib/Dancer/Plugin/Auth/Extensible/Provider/Base.pm
+++ lib/Dancer/Plugin/Auth/Extensible/Provider/Base.pm
@@ -1,10 +1,23 @@
 package Dancer::Plugin::Auth::Extensible::Provider::Base;
 
-# Base class for authentication providers; provides a constructor and handles
-# remembering the realm-specific settings we're using.
-#
-# (There could be multiple realms, using the same provider but differently
-# configured, so each realm will have its own auth provider instance.)
+use Crypt::SaltedHash;
+
+=head1 NAME
+
+Dancer::Plugin::Auth::Extensible::Provider::Base
+
+=head1 DESCRIPTION
+
+Base class for authentication providers.  Provides a constructor which handles
+receiving the realm settings and returning an instance of the provider.
+
+Also provides secure password matching which automatically handles crypted
+passwords via Crypt::SaltedHash.
+
+Finally, provides the methods which providers must override with their
+implementation, which will die if they are not overriden.
+
+=cut
 
 sub new {
     my ($class, $realm) = @_;
@@ -14,4 +27,53 @@ sub new {
     return bless $self => $class;
 }
 
+sub realm_settings { shift->{realm_settings} }
+
+
+ 
+sub match_password {
+    my ($self, $given, $correct) = @_;
+
+    # TODO: perhaps we should accept a configuration option to state whether
+    # passwords are crypted or not, rather than guessing by looking for the
+    # {...} tag at the start.
+    # I wanted to let it try straightforward comparison first, then try
+    # Crypt::SaltedHash->validate, but that has a weakness: if a list of hashed
+    # passwords got leaked, you could use the hashed password *as it is* to log
+    # in, rather than cracking it first.  That's obviously Not Fucking Good.
+    # TODO: think about this more.  This shit is important.  I'm thinking a
+    # config option to indicate whether passwords are crypted - yes, no, auto
+    # (where auto would do the current guesswork, and yes/no would just do as
+    # told.)
+    if ($correct =~ /^{.+}/) {
+        # Looks like a crypted password starting with the scheme, so try to
+        # validate it with Crypt::SaltedHash:
+        return Crypt::SaltedHash->validate($correct, $given);
+    } else {
+        # Straightforward comparison, then:
+        return $given eq $correct;
+    }
+}
+
+
+# Install basic method placeholders which will blow up if the provider module
+# did not implement their own version. 
+{
+    no strict 'refs';
+    for my $method (qw(
+        authenticate_user
+        get_user_details
+        get_user_roles
+        ))
+    {
+        *$method = sub {
+            die "$method was not implemented by provider " . __PACKAGE__;
+        };
+    }
+}
+
+
+
+
+1;
 
