fc7ab5b6173a8eb30dcb4ce9facaa1fdf827e497
Stefan Hornburg (Racke) <racke@linuxia.de>
2014-10-29 18:46:41 +0100

    Fix typo in documentation for generating hashed passwords.

--- lib/Dancer/Plugin/Auth/Extensible.pm
+++ lib/Dancer/Plugin/Auth/Extensible.pm
@@ -33,7 +33,7 @@ methods/sources (config file, database, Unix system users, etc).
 Designed to support multiple authentication realms and to be as extensible as
 possible, and to make secure password handling easy.  The base class for auth
 providers makes handling C<RFC2307>-style hashed passwords really simple, so you
-have no excuse for storing plain-text passwords.  A simple script to ganerate
+have no excuse for storing plain-text passwords.  A simple script to generate
 RFC2307-style hashed passwords is included, or you can use L<Crypt::SaltedHash>
 yourself to do so, or use the C<slappasswd> utility if you have it installed.
 
